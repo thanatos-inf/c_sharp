4118bddb6a1f71cb1844ed87a8868ae4168109ac
thanatos_inf <thanatos_inf@mail.ru>
2022-06-23 12:23:23 +0300

    actually happened

--- Example7_Magic/Program.cs
+++ Example7_Magic/Program.cs
@@ -19,22 +19,22 @@ int x = xa, y = xb;
 
 int count = 0;
 
-while (count < 10)
+while (count < 10000)
 {
     int what = new Random().Next(0, 3); // [0, 3)
-    if(what == 0);
+    if(what == 0)
     {
         x = (x + xa) / 2;
         y = (y + ya) / 2;
     }
 
-     if(what == 1);
+     if(what == 1)
     {
         x = (x + xb) / 2;
         y = (y + yb) / 2;
     }
 
-     if(what == 2);
+     if(what == 2)
     {
         x = (x + xc) / 2;
         y = (y + yc) / 2;
