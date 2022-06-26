5f627102f2cd99de88421c9886e896c0305ffc1b
thanatos_inf <thanatos_inf@mail.ru>
2022-06-26 22:19:45 +0300

    example 10

---
+++ Example10_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 12, 14, 41, 56, 78, 14, 82, 9 };
+
+int n = array.Length;
+int find = 14;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
